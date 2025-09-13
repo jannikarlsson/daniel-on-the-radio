db2c7b5346ef3a65fefc79f361c5c566588e6533
jannikarlsson <jannikarlsson@gmail.com>
2025-09-13 13:16:18 +0200

    Move song logic into custom hook

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,68 +1,9 @@
-import { useEffect, useState } from "react";
-import { fetchEpisodes, fetchProgramList } from "../services/SongService";
-import { ISong, IProgram, IEpisode } from "../models/interfaces"
-import { extractDateTime, getTime, getDate, thirtyDayDiff } from "../utils";
+import { ISong } from "../models/interfaces"
+import { getTime, thirtyDayDiff } from "../utils";
+import { useSong } from "../hooks/useSong";
 
 function Song({ song }: {song: ISong}) {
-
-    const [program, setProgram] = useState<IProgram|null>(null);
-    const [episode, setEpisode] = useState<IEpisode|null>(null);
-    const [startTime, setStartTime] = useState<number>(0);
-    const selectedDate: string | null = getDate(song.starttimeutc);
-
-    const getPrograms = async () => {
-        if (song.channelId && selectedDate) {
-            try {
-                const result: IProgram[] = await fetchProgramList(song.channelId, selectedDate);
-                const songStart = extractDateTime(song.starttimeutc);
-                result.forEach((program: IProgram) => {
-                    const start = extractDateTime(program.starttimeutc);
-                    const end = extractDateTime(program.endtimeutc);
-                    if (songStart > start && songStart < end) {
-                        setProgram(program)
-                    }
-                });
-                
-            } catch (error) {
-                console.error("Error fetching program list:", error);
-            }
-        }
-    }
-
-    const getSound = async () => {
-        if (program?.program.id && song.starttimeutc) {
-            try {
-                if (selectedDate) {
-                    const episodeList: IEpisode[] = await fetchEpisodes(program.program.id, selectedDate);
-                    const episode = episodeList.find(episode => episode.id === program.episodeid);
-                    if (episode) setEpisode(episode);
-                }
-                
-            } catch (error) {
-                console.error("Error fetching program list:", error);
-            }
-        }
-    }
-
-    useEffect(() => {
-        getPrograms();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    useEffect(() => {
-        if (program) getSound();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [program])
-
-    useEffect(() => {
-        if (episode) {
-            const songStart = extractDateTime(song.starttimeutc);
-            const episodeStart = extractDateTime(episode?.broadcasttime.starttimeutc);
-            const offsetInSeconds = (songStart - episodeStart)/1000;
-            setStartTime(offsetInSeconds);
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [episode])
+    const { program, episode, startTime, selectedDate } = useSong(song);
 
     return (
         <div className="notification is-success is-light">
